bb86d54043282be826f05dffe96724e96ca7daa3
safayet nur <safayetnurelectra@gmail.com>
2025-03-16 16:07:54 +0600

    hero section responsive

--- src/component/PageComponent/HeroSection/HeroSection.jsx
+++ src/component/PageComponent/HeroSection/HeroSection.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { IoSearch } from "react-icons/io5";
-
-const Hero = () => {
+import "./hero.css";
+const HeroSection = () => {
   return (
-    <div className="heros">
+    <div className="hero_section">
       <div className="container">
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-2">
           <div className="">
-            <p className="hero_headText font-impact">
+            <p className="hero_headText font-impact text-center lg:text-start md:text-start lg:px-0 md:px-0 px-10 md:w-[500px]">
               Prescription Treatments For Your <span>Health Goals</span>
             </p>
             <button className="hero_btn" type="button">
@@ -35,4 +35,4 @@ const Hero = () => {
   );
 };
 
-export default Hero;
+export default HeroSection;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Hero from "../component/PageComponent/Hero";
 import Category from "../component/PageComponent/Category";
 import HHoo from "../component/PageComponent/HHoo";
 import SupportSection from "../component/PageComponent/SupportSection";
 import LastSection from "../component/PageComponent/LastSection";
+import HeroSection from "../component/PageComponent/HeroSection/HeroSection";
 
 const HomePage = () => {
   return (
     <div className="h-[100vh]">
-      {/* <Hero /> */}
+      <HeroSection />
       {/* <Category /> */}
       {/* <LastSection /> */}
       {/* <HHoo /> */}
